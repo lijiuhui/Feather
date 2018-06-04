15df19d93b609c48a93caa5f58b01b95b46dbf46
dydhyh <dydhyh@163.com>
2018-06-04 21:51:23 +0800

    Request在初始化完成之后， 移除相关超全局变量。

--- Core/Request.php
+++ Core/Request.php
@@ -15,12 +15,13 @@ class Request
     protected $domain;
     private $view;
     private $data;
+    private $globals = array('_POST', '_GET', '_FILES', '_SERVER');
 
     public function __construct()
     {
         $this->view = new View();
         $this->data = new Domains\RequestData();
-        $this->parse_data();
+        $this->init();
     }
 
     public function assign(string $name, $data)
@@ -33,11 +34,31 @@ class Request
         $this->view->display($view_path);
     }
 
+    private function init()
+    {
+        $this->parse_data();
+        $this->remove_globals();
+    }
+
     private function parse_data()
     {
         $this->data->set_payloads($_GET);
         $this->data->set_form_datas($_POST);
         $this->data->set_files($_FILES);
         $this->data->set_servers($_SERVER);
+    }
+
+    /**
+     * 移除已经转换的超全局变量
+     */
+    private function remove_globals()
+    {
+        foreach($this->globals as $item)
+        {
+            foreach($GLOBALS[$item] as $key => $value)
+            {
+                unset($GLOBALS[$key]);
+            }
+        }
     }
 }
